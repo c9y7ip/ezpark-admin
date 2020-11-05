052f84a93af3e6863b4e29ac2ea238cefe46a1c5
jorawarc <jorawarc@sfu.ca>
2020-11-04 20:19:27 -0800

    post login info to server

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,23 +1,49 @@
 import React from 'react';
 
-const LoginPage = () => {
-    return (
-        <section>
-            <h2>Login Page</h2>
-            <form>
-                <section>
-                    <label>Email : </label>
-                    <input type='Email' placeholder='example@email.com'/>
-                </section>
-                <section>
-                    <label>Password : </label>
-                    <input type='password' placeholder='*****'/>
-                </section>
-                <button>Login</button>
-            </form>
-        </section>
-    );
-};
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {email: '', password: ''};
+    }
+
+    onPasswordChange(e){
+        this.setState({password: e.target.value});
+    }
+
+    onEmailChange(e){
+        this.setState({email: e.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state);
+        fetch('http://localhost:5000/auth/login', {
+            method: 'post',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then((resp)=>{ return resp.text() }).then((text)=>{ console.log(text) });
+    }
+
+    render(){
+        return (
+            <section>
+                <h2>Login Page</h2>
+                <form onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <section>
+                        <label>Email : </label>
+                        <input onChange={this.onEmailChange.bind(this)} type='email' placeholder='example@email.com'/><br />
+                        <label>Password : </label>
+                        <input onChange={this.onPasswordChange.bind(this)} type='password' placeholder='*****'/>
+                    </section>
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+        );
+    }
+}
 
 
 export default LoginPage;
