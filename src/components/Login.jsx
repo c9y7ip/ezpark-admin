8133c1846ab9a03d94b0376db113e10846d38ef5
lgoldber <lgoldber@sfu.ca>
2020-11-07 13:58:41 -0800

    fix event handling in login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,25 +5,33 @@ import EventService from "../services/EventService";
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {email: '', password: '', isAuthenticated: false}; // NOTE: user should always be intended to be
-                                                                        // de-authenticated when they redirect to login page
+        this.state = { email: '', password: '', isAuthenticated: false }; // NOTE: user should always be intended to be
+        // de-authenticated when they redirect to login page
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
     }
 
+    onPasswordChange(e) {
+        this.setState({ password: e.target.value });
+    }
 
+    onEmailChange(e) {
+        this.setState({ email: e.target.value });
+    }
 
-    handleSubmit(event){
+    handleSubmit(event) {
         event.preventDefault();
 
-        const email = Document.getElementById("email").value
-        const password = Document.getElementById("password").value
+        var password = this.state.password
+        var email = this.state.email
 
         EventService.auth.userLogin({ // include only what you need in the payload
             email,
             password
         }, (authSuccess) => {
-            this.setState({isAuthenticated: authSuccess});
-            if(authSuccess){
+            this.setState({ isAuthenticated: authSuccess });
+            if (authSuccess) {
                 this.props.history.push('/', this.state);
             } else {
                 window.location.reload(false);
@@ -38,11 +46,11 @@ class Login extends React.Component {
                 <form onSubmit={this.handleSubmit} method="POST">
                     <section>
                         <label>Email : </label>
-                        <input id="email" type='email' placeholder='example@email.com'/>
+                        <input id="email" type='email' placeholder='example@email.com' />
                     </section>
                     <section>
                         <label>Password : </label>
-                        <input id="password" type='password' placeholder='*****'/>
+                        <input id="password" type='password' placeholder='*****' />
                     </section>
                     <button type='submit'>Login</button>
                 </form>
