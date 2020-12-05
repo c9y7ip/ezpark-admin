05f2b2609257d07bfb4d032a1b62050ad8af8f18
lgoldber <lgoldber@sfu.ca>
2020-12-04 17:52:20 -0800

    changed event service host

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,28 +10,15 @@ import EventService from "../services/EventService";
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { allParkingMap: {}, allLicenses: [] }
+        this.state = { allParkingMap: {} }
         console.log(this.props.allParkingMap)
     }
 
     componentDidMount = () => {
-        this.getLicenses()
         console.log(this.props.allParkingMap)
         this.setState({
             allParkingMap: this.props.allParkingMap
         })
-    }
-
-    getLicenses = () => {
-        EventService.apiClient.get(
-            '/car/all-licenses')
-            .then((res) => {
-                this.setState({ allLicenses: res.data });
-                console.log(this.state.allLicenses)
-            })
-            .catch(e => {
-                console.log(e);
-            });
     }
 
     componentWillReceiveProps(nextProps) {
@@ -49,7 +36,7 @@ class Home extends React.Component {
                 <section className="center96">
                     <Tabs defaultActiveKey="parking-lot">
                         <Tab eventKey="validate-license" title="Validate License">
-                            <ValidateLicense allLicenses={this.state.allLicenses} />
+                            <ValidateLicense />
                         </Tab>
                         <Tab eventKey="parking-lot" title="Parking Lots" >
                             <ParkingList allParkingMap={this.props.allParkingMap} deleteLot={this.props.deleteLot} />
